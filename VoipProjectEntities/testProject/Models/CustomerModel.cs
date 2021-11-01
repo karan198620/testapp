833998ea53402fd5bf54f465e5cf0dc6f0781c04
Anagha Thorat <t.anagha8@gmail.com>
2021-11-01 14:52:35 +0530

    Get all customers

---
+++ VoipProjectEntities/testProject/Models/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testProject.Models
+{
+    public class CustomerModel
+    {
+        public Guid CustomerId { get; set; }
+
+        [Required]
+        public string CustomerName { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public bool ISMigrated { get; set; }
+        public int CustomerTypeID { get; set; } //enum
+        public bool ISTrialBalanceOpted { get; set; }
+    }
+}
